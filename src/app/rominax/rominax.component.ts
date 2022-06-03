4455ea8212a5e526b4070b0a1b7e1c5beacd322f
RominaBF <romina.bfarimani@gmail.com>
2022-06-03 11:40:10 +0200

    first ronix commit brnch

---
+++ src/app/rominax/rominax.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-rominax',
+  templateUrl: './rominax.component.html',
+  styleUrls: ['./rominax.component.css']
+})
+export class RominaxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
